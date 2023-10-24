68070787eeff406001b29e6ec39405b3d30f3deb
saksham-raj-RS <148848563+saksham-raj-RS@users.noreply.github.com>
2023-10-24 12:42:42 +0530

    Add files via upload

---
+++ p1_hello.java
@@ -0,0 +1,27 @@
+// hello everyone , today we will be doing the program of hello world in java language
+// first of all we have to create a class of the same name of that of file.
+// after that we have to create a main function inside the public class .
+// after that we will be giving the command to print the Hello World.
+// Lets begin. . .
+public class p1_hello{
+    //as mentioned above the name of the class is as same as the file name you can see
+    //you can make this class as public class
+
+    //creating the main function 
+    public static void main(String args[]){
+
+        //giving command to print the Hello World
+
+        System.out.print("Hello World!");
+        // now every this is done 
+        // Lets run the program
+
+        //you can see below the output Hello World! have come in the output.
+        //thats all for todays program.
+        //Thank you
+
+        // all the code is uploaded to the github repositry 
+        //the link will be in the description box.
+        // thank you
+    }
+}
